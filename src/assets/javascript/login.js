1d32485fff63162b4c3e0b9cd7f8b648b9cda302
GabrielGapsk <gabriel-gapski@outlook.com>
2022-09-28 16:44:36 -0300

    repositorio

---
+++ src/assets/javascript/login.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    
+
+    $("#signin").click( function(){
+        let email = $("#email").val();
+        let senha = $("#senha").val();
+
+        console.log( validarLogin(email, senha) );
+
+        if( validarLogin(email, senha ) ) {
+
+            alert("Preenchido corretamente")
+        } else {
+            alert("Preencher todos os campos")
+        }
+
+    } )
+
+
+    function validarLogin(e, s) {
+        if( e == "" || s == "" ){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+
+});
+
+
+
