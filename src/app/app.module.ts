522fbb5cc825c3ba316037ab94c456f0575a2ce8
GabrielGapsk <gabriel-gapski@outlook.com>
2022-10-05 17:29:28 -0300

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FormsModule } from "@angular/forms";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,8 +1,8 @@
 export class User {
 
     constructor(
-        public nome?: string,//undefined
-        public idade?: number,
+       // public nome?: string,//undefined
+      //  public idade?: number,
         public email?: string,
         public password?:string
     ){}
